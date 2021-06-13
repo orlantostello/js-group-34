62468e3122974c687c8ed2399ecbbe89d8508363
Oleg Teslenko <orlantostello@gmail.com>
2021-06-14 00:53:12 +0300

    add

--- src/index.js
+++ src/index.js
@@ -187,16 +187,48 @@ const getUsersWithAge = (users, minAge, maxAge) => {
 };
 
 // ============ Задание 20 ===========
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter((user) => {
+    if (user.friends.includes(friendName)) {
+      return user;
+    }
+  });
+};
 
 // ============ Задание 21 ===========
+const getFriends = (users) => {
+  return users
+    .flatMap((user) => user.friends)
+    .filter((elem, index, arr) => arr.indexOf(elem) === index);
+};
 
 // ============ Задание 22 ===========
+const getActiveUsers = (users) => {
+  return users.filter((user) => user.isActive === true);
+};
 
 // ============ Задание 23 ===========
+const getInactiveUsers = (users) => {
+  return users.filter((user) => user.isActive !== true);
+};
 
 // ============ Задание 24 ===========
+const books = [
+  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
+  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
+  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
+  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
+];
+const BOOK_TITLE = "Сон смешного человека";
+const AUTHOR = "Роберт Шекли";
+
+const bookWithTitle = books.find((book) => book.title === BOOK_TITLE);
+const bookByAuthor = books.find((book) => book.author === AUTHOR);
 
 // ============ Задание 25 ===========
+const getUserWithEmail = (users, email) => {
+  return users.find((user) => user.email === email);
+};
 
 // ============ Задание 26 ===========
 
@@ -235,86 +267,3 @@ const getUsersWithAge = (users, minAge, maxAge) => {
 // ============ Задание 43 ===========
 
 // ============ Задание 44 ===========
-
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male",
-    age: 37,
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female",
-    age: 34,
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male",
-    age: 24,
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female",
-    age: 21,
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male",
-    age: 27,
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male",
-    age: 38,
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female",
-    age: 39,
-  },
-];
-
-const getUsersWithFriend = (users, friendName) => {
-  return users.filter((user) => {
-    if (user.friends.includes(friendName)) {
-      return user.name;
-    }
-  });
-};
-
-console.log(getUsersWithFriend(users, "Adrian Cross"));
